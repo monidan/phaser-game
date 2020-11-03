1f307be61636ccc7974defefd615072f8e78c107
monidan <daniil17032002@gmail.com>
2020-11-03 19:23:12 +0200

    Changed ball respawn. PModiffied score usage

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,8 +5,6 @@ export default class Game extends Phaser.Scene {
     init() {
         this.aiPlatformVelocity = new Phaser.Math.Vector2(0, 0);
         this.scoreNumber = 0;
-        this.ballVelocityX = Phaser.Math.Between(-200, 200);
-        this.ballVelocityY = Phaser.Math.Between(-200, 200);
     }
 
     preload() {
@@ -14,13 +12,18 @@ export default class Game extends Phaser.Scene {
     }
 
     create() {
+        this.physics.world.setBounds(-100, 0, 1000, 500)
+
         this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
         this.physics.add.existing(this.ball);
         this.ball.body.setBounce(1, 1)
 
+        const angle = Phaser.Math.Between(0, 360);
+        const vec = this.physics.velocityFromAngle(angle, 200);
+
         this.ball.body.setCollideWorldBounds(true, 1, 1)
 
-        this.ball.body.setVelocity(this.ballVelocityX, this.ballVelocityY)
+        this.ball.body.setVelocity(vec.x, vec.y)
 
         this.platform = this.add.rectangle(50, 250, 20, 100, 0xFFFFFF, 1)
         this.physics.add.existing(this.platform, true);
@@ -48,7 +51,7 @@ export default class Game extends Phaser.Scene {
             return
         }
 
-        const aiSpeed = 3;
+        const aiSpeed = 2;
         if(diff < 0) {
             this.aiPlatformVelocity.y = -aiSpeed;
             if(this.aiPlatformVelocity.y < -10){
@@ -64,9 +67,24 @@ export default class Game extends Phaser.Scene {
         this.aiPlatform.y += this.aiPlatformVelocity.y;
         this.aiPlatform.body.updateFromGameObject();
 
-        if(this.ball.body.x === 800){
+        if(this.ball.body.x > 830){
             this.scoreNumber++;
             this.score.setText(`Score: ${this.scoreNumber}`);
+            
+            this.resetBall()
+        } else if(this.ball.x < -30) {
+            this.scoreNumber--;
+            this.score.setText(`Score: ${this.scoreNumber}`);
+            this.resetBall();
         }
+    }
+
+    resetBall() {
+        this.ball.setPosition(400, 250)
+
+        const angle = Phaser.Math.Between(0, 360);
+        const vec = this.physics.velocityFromAngle(angle, 200);
+
+        this.ball.body.setVelocity(vec.x, vec.y)
     }
 } 
